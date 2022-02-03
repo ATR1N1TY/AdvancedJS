663674ad523f6aab2931bfacad9e083c8de5a431
unknown <saba4inasaridze@gmail.com>
2022-02-03 12:31:52 +0400

    Q5: what is DOM explained shortly

---
+++ DAY2.js
@@ -0,0 +1,20 @@
+///<<<DAY 2>>>///
+
+//Todays questions:
+//5. What is the  DOM?
+//6. What is Event Propagation?
+//7. What's Event Bubbling?
+//8. What's Event Capturing?
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Q5:
+
+//DOM aka Document Object Model is an API(aplication programming interface), which allows us
+// to manipulate HTML documents with javascript code
+// document means HTML document itself
+// Object means tags in HTML document, Bassically JS treats HTML tags like objects and also
+// JS is known to be good with objects
+// Model means how these elements are nested
+
+//with DOM we can add remove and change insides of an HTML element with JS code
+
+// tags in html is also called nodes
