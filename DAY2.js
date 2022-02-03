5552a0778768ed4535e7277649517355a67152a4
unknown <saba4inasaridze@gmail.com>
2022-02-03 13:29:08 +0400

    Q7: what is event bubbling

--- DAY2.js
+++ DAY2.js
@@ -19,16 +19,22 @@
 
 // tags in html is also called nodes
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Q6 Q7 Q8:
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Q6:
 
 //Event propagation is the way how the event capturing flows
 
 //when we have nested tags in HTML document by default if we click on the inner most element
 // events will flow up(bubble up) also called bubbling, to the body > html > document
 
-//what if we don't want to bubble events?
-// we can simply prevent event bubbling by typing e.stopPropagation()
+//or if event capturing is true and click on inner most element,
+//events will flow down from parent to child elements till event will be catched
 
-//Event capturing is when we click somehting on a webpage this means event was captured
-//Event can be anythin: keyUp, keyDown, click, etc.
-//when Event is detected on a webpage by EventListener, this means event was captured
+//in eather way, what if we don't want to bubble events? or stop event capturing?
+// we can simply prevent event bubbling/capturing by typing e.stopPropagation()
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Q7:
+
+//When we have nested elements in HTML, and eventListener is added to last child element
+//if we click that element, sequence of events will occur from child to parent(will bubble up)
+//that's becouse this elements are nested and if we click on last child, we have also clicked
+// on parent element of that child and so on
