f31ed3ca88cfe3c729a582a73f416f09691fa342
unknown <saba4inasaridze@gmail.com>
2022-02-05 14:22:05 +0400

    Q14: Difference between == and ===

--- DAY4.js
+++ DAY4.js
@@ -16,3 +16,28 @@
 
 //e.target will return that div where the event occured
 //but e.currentTarget will return in which EventListener have been occured, meaning body.
+
+//14.
+
+// ==(double equals) is abstract comparison operator,
+// which means that before comparing to second vaule
+//  it converts operands to same type
+
+//while ===(tripple equals) is strict comparison operator and doesn't convert anything
+
+//code bellow will return true
+if (1 == "1") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//but this code with tripple equals will return false, as it's strict comparision operator
+if (1 === "1") {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//Mostly we use ===(tripple equals), becouse most of the time we don't want to convert anything
+//but there are some cases when ==(double equals) is usefull too!
