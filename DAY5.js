bc866d9c43ebe4c4c8261b22c7a2871a71ae0263
unknown <saba4inasaridze@gmail.com>
2022-02-09 23:23:37 +0400

    Q20: What are Closures

--- DAY5.js
+++ DAY5.js
@@ -78,3 +78,11 @@ console.log(blockVar); // this will throw an error because blockVar doesn't exis
 //in JavaScript!.
 
 //20.
+
+//A closure is the combination of a function bundled together (enclosed)
+// with references to its surrounding state (the lexical environment).
+
+//In other words, a closure gives you access to an outer function’s
+// scope from an inner function.
+//In JavaScript, closures are created every time a function is created,
+// at function creation time.
